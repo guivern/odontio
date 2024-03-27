c7d286594c2144334645209cc9f127169fd3c580
Guillermo Verón <guiver95@gmail.com>
2024-03-27 10:40:51 -0400

    Add DeletePaymentValidator

---
+++ Odontio.Application/Payments/Commands/DeletePayment/DeletePaymentValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace Odontio.Application.Payments.Commands.DeletePayment;
+
+public class DeletePaymentValidator : AbstractValidator<DeletePaymentCommand>
+{
+    public DeletePaymentValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+        RuleFor(x => x.WorkspaceId).NotEmpty();
+        RuleFor(x => x.PatientId).NotEmpty();
+    }
+}
